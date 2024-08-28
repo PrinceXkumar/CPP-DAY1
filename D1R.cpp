cf7f58df2fc550cd2a6b64df5d137b1058df99e2
Prince Kumar <118840584+PrinceXkumar@users.noreply.github.com>
2024-08-28 22:31:01 +0530

    Add files via upload
    In this file I read about what is C++ language Syntax, variable, identifiers and how to take input and how to print the output in C++

---
+++ D1R.cpp
@@ -0,0 +1,69 @@
+// Take a integer and assign value as 10 and print out
+/*
+#include<iostream>
+using namespace std;
+int  main(){
+	
+	int a;
+	a=10;
+	cout<<"Output is : "<<a<<endl;
+	return 0;
+}
+*/
+
+
+//made by bjorne in 1983
+//it is extension of c language and it is cabible for making games , application , and software and many more 
+//it also provide the opps concept for using the class and object 
+
+//Q2   take three int variable with same value 
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b, c;
+	a=b=c=10;
+	cout<<a+b+c<<endl;
+	return 0;
+	
+}
+
+*/
+
+// DATA TYPE IN C++
+//mainly we have int , char , double, string, float, bool data type in c++
+
+// How to take input in  c++
+// we have cin object to take iunput in c++
+//syntax : cin>>variable ;
+//(>>) - insertion operator and (<<)- extraction operator
+
+  //   Q-3   Make a simple calculator and take input from keyboard
+  
+  /*
+  #include<iostream>
+  using namespace std;
+int main(){
+	int	a, b;
+	cout<<"The number is :";
+	cin>>a>>b;
+	cout<<"Sum is : "<<a+b<<endl;
+	cout<<"Sub is : "<<a-b<<endl;
+	cout<<"Multi : "<<a*b<<endl;
+	cout<<"division  : "<<a/b<<endl;
+	return 0;
+}  
+
+*/
+
+
+
+
+
+
+
+
+
+
+
